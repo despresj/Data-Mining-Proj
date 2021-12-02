a2f51326371c68b5a5c10429fe29d801a6ca42e9
despresj <despresj@mail.gvsu.edu>
2021-12-01 20:36:19 -0500

    fixed labels

--- R/plots.R
+++ R/plots.R
@@ -23,20 +23,21 @@ plot_df <- bind_rows(errors, store_sales) %>%
 
 my_saver <- function (name) {
   ggsave(filename = paste0("R/saved_plots/", name, ".png"),
-         width = 16, height = 9)
+         width = 16, height = 9, dpi = 320)
 }
 
 base_plot <- plot_df %>%
   filter(date <= test_date) %>% 
   ggplot((aes(x = date, y = sales))) +
   geom_line(color = "grey44") + 
+  expand_limits(y=0) + 
   scale_x_date(
     date_breaks = "7 month",
     date_labels = "%b '%y",
     limits = c(min(store_sales$date), max(store_sales$date))) +
   labs(title = "Daily Sales Training Data", 
        subtitle = " ",
-       x = NULL, y = "Units Sold")
+       x = NULL, y = "Units Sold of Product 1 / 500")
 
 base_plot
 
@@ -59,6 +60,7 @@ plots_fct <- base_plot +
     data = subset(plot_df),
     aes(x = date, y = pred_sales), color = "blue", size = 0.25) +
   labs(title = "Daily Sales Point Forecasts") +
+  scale_y_continuous(limits = c(0, 50)) + 
   facet_wrap(~model, ncol = 2)+
   theme(axis.title.x=element_blank(),
         axis.text.x=element_blank(),
@@ -72,6 +74,6 @@ plots_fct +
   geom_line(data = filter(plot_df, date > test_date), 
             aes(x = date, y = sales),
                       color = "darkred", alpha = 0.5) +
-  labs(title = "Daily Sales Point Forecasts With Testing Set") +
+  labs(title = "Daily Sales Point Forecasts With Testing Set") 
   
 my_saver("point_fcast_test")
